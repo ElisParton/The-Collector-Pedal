0f0c6f0f229ca4531d1a66676935021e735805e5
Elis <eyqp2008@gmail.com>
2025-11-30 22:43:24 +1100

    Multiplication, Addition, Subtraction and Printing implemented correctly. Dubugger also setup

--- Matrix_maths.c
+++ Matrix_maths.c
@@ -7,10 +7,11 @@ typedef struct Matrix {
     int rows; /// Rows in the Matrix
     int cols; /// Collumns in the Matrix
 } Matrix;
+
 /**A function to create a matrix from a buffer.
  * Ensure that the buffer is the correct size before calling this function.
 */
-Matrix create_matrix(int row, int col, double* buffer, int buf_len) {
+Matrix matrix_create(int row, int col, double* buffer, int buf_len) {
     assert(buffer != NULL);
     assert(col > 0);
     assert(row > 0);
@@ -23,46 +24,109 @@ Matrix create_matrix(int row, int col, double* buffer, int buf_len) {
     return m;
 }
 
-static inline double get_matrix_entry(const Matrix* m, int r, int c) {
-    return m->entries[r * m->cols + c];
+static inline double matrix_get_entry(const Matrix m, int r, int c) {
+    return m.entries[r * m.cols + c];
 }
 
-static inline void set_matrix_entry(Matrix* m, int r, int c, double v) {
-    m->entries[r * m->cols + c] = v;
+static inline void matrix_set_entry(Matrix m, int r, int c, double v) {
+    m.entries[r * m.cols + c] = v;
 }
 
-Matrix matrix_multiplication(const Matrix* A, const Matrix* B, double* buffer, int buf_len) {
-    assert(A != NULL);
-    assert(B != NULL);
-    assert(A->cols == B->rows);
-    Matrix C = create_matrix(A->rows, B->cols, buffer, buf_len);   
+void matrix_print(Matrix A) {
+    printf("Matrix (%d x %d):\n", A.rows, A.cols);
+    for (int r = 0; r < A.rows; r++) {
+        for (int c = 0; c < A.cols; c++) {
+            double val = matrix_get_entry(A, r, c);
+            printf("%f ", val); 
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
-    for (int r = 0; r < A->rows; r++) {
-        for (int c = 0; c < B->cols; c++) {
+Matrix matrix_multiplication(const Matrix A, const Matrix B, double* buffer, int buf_len) {
+    //assert(A != NULL);
+    //assert(B != NULL);
+    //assert(A->cols == B->rows);
 
+    Matrix C = matrix_create(A.rows, B.cols, buffer, buf_len);   
+
+    for (int r = 0; r < A.rows; r++) {
+        for (int c = 0; c < B.cols; c++) {
             double sum = 0.0;
-
-            for (int k = 0; k < A->cols; k++) {
-                sum += get(A, r, k) * get(B, k, c);
+            for (int k = 0; k < A.cols; k++) {
+                sum += matrix_get_entry(A, r, k) * matrix_get_entry(B, k, c);
             }
-
-            set(C, r, c, sum);
+            matrix_set_entry(C, r, c, sum);
         }
     }
     return C;
 }
 
-Matrix matrix_addition(const Matrix* A, const Matrix* B, double* buffer, int buf_len) {
-    assert(A != NULL);
-    assert(B != NULL);
-    assert(A->cols == B->cols);
-    assert(A->rows == B->rows);
-    Matrix C = create_matrix(A->rows, A->cols, buffer, buf_len);
-    for (int r = 0; r < A->rows; r++) {
-        for (int c = 0; c < A->cols; c++) {
-            double sum = get(A, r, c) + get(B, r, c);
-            set(C, r, c, sum);
+Matrix matrix_addition(const Matrix A, const Matrix B, double* buffer, int buf_len) {
+    //assert(A != NULL);
+    //assert(B != NULL);
+    //assert(A->cols == B->cols);
+    //assert(A->rows == B->rows);
+
+    Matrix C = matrix_create(A.rows, A.cols, buffer, buf_len);
+
+    for (int r = 0; r < A.rows; r++) {
+        for (int c = 0; c < A.cols; c++) {
+            double sum = matrix_get_entry(A, r, c) + matrix_get_entry(B, r, c);
+            matrix_set_entry(C, r, c, sum);
         }
     }
+    return C;
 }
 
+Matrix matrix_subtraction(const Matrix A, const Matrix B, double* buffer, int buf_len) {
+    
+    //assert(A != NULL);
+    //assert(B != NULL);
+    //assert(A->cols == B->cols);
+    //assert(A->rows == B->rows);
+    
+    
+    Matrix C = matrix_create(A.rows, A.cols, buffer, buf_len);
+
+    for (int r = 0; r < A.rows; r++) {
+        for (int c = 0; c < A.cols; c++) {
+            double difference = matrix_get_entry(A, r, c) - matrix_get_entry(B, r, c);
+            matrix_set_entry(C, r, c, difference);
+        }
+    }
+    return C;
+}
+
+
+int main() 
+{
+    double a_buf[3*3] = {
+    1, 4, 8,
+    3, 5, 4,
+    12, 3, 18
+    };
+
+    double b_buf[3*3] = {
+    2, 7, 4,
+    3, 5, 9,
+    8, 5, 1,
+    };
+
+    Matrix A = matrix_create(3, 3, a_buf, 3*3);
+    Matrix B = matrix_create(3, 3, b_buf, 3*3);
+    matrix_print(A);
+    matrix_print(B);
+    double c_buf[A.rows * B.cols];
+    Matrix C = matrix_multiplication(A, B, c_buf, A.rows * B.cols);
+    matrix_print(C);
+    double d_buf[A.rows * A.cols];
+    Matrix D = matrix_addition(A, B, d_buf, A.rows * A.cols);
+    matrix_print(D);
+    double e_buf[A.rows * A.cols];
+    Matrix E = matrix_subtraction(A, B, e_buf, A.rows * A.cols);
+    matrix_print(E);
+    
+    return 0;
+}
