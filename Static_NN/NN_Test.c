4349051349e7862e23e3b3b613ad9cc9f4919a59
Elis <eyqp2008@gmail.com>
2025-12-01 22:52:22 +1100

    -Implemented activation functions -Improved example NN_Test to complete full forward propagation with activation functions. VLAs used for network config.

--- Static_NN/NN_Test.c
+++ Static_NN/NN_Test.c
@@ -1,34 +1,40 @@
 #include "NN_Tools.c"
-void main() {
-    double inp_buffer1[4] = {1.0, 0.5, -1.5, 2.0};
-    double weight_buffer1[12] = {
-        0.2, -0.5, 0.1, 0.4,
-        -0.3, 0.8, -0.6, 0.7,
-        0.5, -0.2, 0.3, -0.1
-    };
-    double bias_buffer1[3] = {0.1, -0.2, 0.3};
-    double out_buffer1[3];
+#include <stdlib.h>
 
-    double inp_buffer2[3] = {0};
-    double weight_buffer2[6] = {
-        0.3, -0.7, 0.5,
-        -0.4, 0.6, -0.2
-    };
-    double bias_buffer2[2] = {0.0, 0.1};
-    double out_buffer2[2];
-    NN_Layer layer1 = NN_create_layer(4, 3,
+int main(int argc, char *argv[]) {
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    int netconfig[3] = {a, b, c};
+
+    double inp_buffer1[netconfig[0]];
+    double weight_buffer1[netconfig[0]*netconfig[1]];
+    double bias_buffer1[netconfig[1]];
+    double out_buffer1[netconfig[1]];
+
+    double inp_buffer2[netconfig[1]];
+    double weight_buffer2[netconfig[1]*netconfig[2]];
+    double bias_buffer2[netconfig[2]];
+    double out_buffer2[netconfig[2]];
+
+    NN_Layer layer1 = NN_create_layer(a, b,
         inp_buffer1,
         weight_buffer1,
         bias_buffer1,
-        out_buffer1);
-    NN_Layer layer2 = NN_create_layer(3, 2,
+        out_buffer1
+    );
+
+    NN_Layer layer2 = NN_create_layer(b, c,
         inp_buffer2,
         weight_buffer2,
         bias_buffer2,
         out_buffer2);
+
     NN_Layer layers[2] = {layer1, layer2};
     Neural_Network net = NN_create(2, layers);
-    NN_forward_pass(net);
-    matrix_print(net.layers[0].outputs);
-    matrix_print(net.layers[1].outputs);
+    NN_forward_pass(net, NN_Linear);
+    matrix_print(net.layers[0].weights);
+    matrix_print(net.layers[1].weights);
+    return 0;
 }
--- Static_NN/NN_Tools.c
+++ Static_NN/NN_Tools.c
@@ -1,4 +1,6 @@
 #include "Matrix_maths.c"
+#include <stdlib.h>
+#include <math.h>
 
 typedef struct NN_Layer {
     int neurons; // Number of neurons in the layer
@@ -18,6 +20,12 @@ NN_Layer NN_create_layer(int neurons, int outneurons, double* input_buffer, doub
     NN_Layer layer;
     layer.neurons = neurons;
     layer.outneurons = outneurons;
+    for (int i = 0; i < neurons * outneurons; i++) {
+        weight_buffer[i] = (double)rand() / (double)RAND_MAX;; // Initialize weights to zero
+    }
+    for (int i = 0; i < outneurons; i++) {
+        bias_buffer[i] = 0.0; // Initialize biases to zero
+    }
     layer.inputs = matrix_create(neurons, 1, input_buffer);
     layer.weights = matrix_create(outneurons, neurons, weight_buffer);
     layer.biases = matrix_create(outneurons, 1, bias_buffer);
@@ -37,18 +45,35 @@ NN_Layer NN_forward(const NN_Layer layer) {
     Matrix weighted_sum = matrix_multiplication(layer.weights, layer.inputs, temp_buffer);
     for (int i = 0; i < layer.outneurons; i++) {
         double sum = matrix_get_entry(weighted_sum, i, 0) + matrix_get_entry(layer.biases, i, 0);
-        matrix_set_entry(layer.outputs, i, 0, sum); // No activation function applied
+        matrix_set_entry(layer.outputs, i, 0, sum);
     }
     return layer;
 }
 
-Neural_Network NN_forward_pass(const Neural_Network net) {
+Neural_Network NN_forward_pass(const Neural_Network net, int (*activation_func)(double)) {
     Neural_Network current_net = net;
     for (int i = 0; i < net.num_layers; i++) {
         current_net.layers[i] = NN_forward(current_net.layers[i]);
         if (i < net.num_layers - 1) {
-            current_net.layers[i + 1].inputs = current_net.layers[i].outputs;
+            for (int r = 0; r < current_net.layers[i].outneurons; r++) {
+                double activated_value = activation_func(matrix_get_entry(current_net.layers[i].outputs, r, 0));
+                matrix_set_entry(current_net.layers[i + 1].inputs, r, 0, activated_value);
+            }
         }
     }
     return current_net;
 }
+
+double NN_ReLU(double x) {
+    return (x > 0) ? x : 0;
+}
+double NN_Sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+double NN_Tanh(double x) {
+    return tanh(x);
+}
+double NN_Linear(double x) {
+    return x;
+}
+
