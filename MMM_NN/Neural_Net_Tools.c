44cffb63807a2a235fb050af3ddc556cdb2d817f
Elis <eyqp2008@gmail.com>
2025-12-01 17:33:25 +1100

    Added Neural Net tools - Neural_Net, neural_net_create, neural_net_forward

--- MMM_NN/Neural_Net_Tools.c
+++ MMM_NN/Neural_Net_Tools.c
@@ -9,6 +9,11 @@ typedef struct Neural_Net_Layer {
     Matrix outputs; // Output vector for the layer
 } Neural_Net_Layer;
 
+typedef struct Neural_Net {
+    int num_layers; // Number of layers in the neural network
+    Neural_Net_Layer* layers; // Array of layers
+} Neural_Net;
+
 Neural_Net_Layer neural_net_create_layer(int neurons, int outneurons, double* input_buffer, double* weight_buffer, double* bias_buffer, double* output_buffer) {
     Neural_Net_Layer layer;
     layer.neurons = neurons;
@@ -20,6 +25,13 @@ Neural_Net_Layer neural_net_create_layer(int neurons, int outneurons, double* in
     return layer;
 }
 
+Neural_Net neural_net_create(int num_layers, Neural_Net_Layer* layers) {
+    Neural_Net net;
+    net.num_layers = num_layers;
+    net.layers = layers;
+    return net;
+}
+
 Neural_Net_Layer neural_net_forward(const Neural_Net_Layer layer) {
     double temp_buffer[layer.outneurons]; // Temporary buffer for matrix multiplication
     Matrix weighted_sum = matrix_multiplication(layer.weights, layer.inputs, temp_buffer);
@@ -28,4 +40,15 @@ Neural_Net_Layer neural_net_forward(const Neural_Net_Layer layer) {
         matrix_set_entry(layer.outputs, i, 0, sum); // No activation function applied
     }
     return layer;
+}
+
+Neural_Net neural_net_forward_pass(const Neural_Net net) {
+    Neural_Net current_net = net;
+    for (int i = 0; i < net.num_layers; i++) {
+        current_net.layers[i] = neural_net_forward(current_net.layers[i]);
+        if (i < net.num_layers - 1) {
+            current_net.layers[i + 1].inputs = current_net.layers[i].outputs;
+        }
+    }
+    return current_net;
 }
