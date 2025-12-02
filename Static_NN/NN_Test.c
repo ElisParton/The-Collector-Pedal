fe4c374209f39af1b8c7da86988fb12f2138692c
Elis <eyqp2008@gmail.com>
2025-12-02 22:01:59 +1100

    Back propagation - untested

--- Static_NN/NN_Test.c
+++ Static_NN/NN_Test.c
@@ -12,28 +12,37 @@ int main(int argc, char *argv[]) {
     double weight_buffer1[netconfig[0]*netconfig[1]];
     double bias_buffer1[netconfig[1]];
     double out_buffer1[netconfig[1]];
+    double deltas_buffer1[netconfig[1]];
 
     double inp_buffer2[netconfig[1]];
     double weight_buffer2[netconfig[1]*netconfig[2]];
     double bias_buffer2[netconfig[2]];
     double out_buffer2[netconfig[2]];
+    double deltas_buffer2[netconfig[2]];
 
     NN_Layer layer1 = NN_create_layer(a, b,
         inp_buffer1,
         weight_buffer1,
         bias_buffer1,
-        out_buffer1
+        out_buffer1,
+        deltas_buffer1,
+        Linear,
+        Linear_1
     );
 
     NN_Layer layer2 = NN_create_layer(b, c,
         inp_buffer2,
         weight_buffer2,
         bias_buffer2,
-        out_buffer2);
+        out_buffer2,
+        deltas_buffer2,
+        Linear,
+        Linear_1
+    );
 
     NN_Layer layers[2] = {layer1, layer2};
     Neural_Network net = NN_create(2, layers);
-    NN_forward_pass(net, NN_Linear);
+    NN_forward_pass(net);
     matrix_print(net.layers[0].weights);
     matrix_print(net.layers[1].weights);
     return 0;
--- Static_NN/NN_Tools.c
+++ Static_NN/NN_Tools.c
@@ -19,7 +19,7 @@ typedef struct Neural_Network {
     NN_Layer* layers; // Array of layers
 } Neural_Network;
 
-NN_Layer NN_create_layer(int neurons, int outneurons, double* input_buffer, double* weight_buffer, double* bias_buffer, double* output_buffer) {
+NN_Layer NN_create_layer(int neurons, int outneurons, double* input_buffer, double* weight_buffer, double* bias_buffer, double* output_buffer, double* deltas_buffer,double (*activation)(double), double (*activation_derivative)(double)) {
     NN_Layer layer;
     layer.neurons = neurons;
     layer.outneurons = outneurons;
@@ -33,6 +33,8 @@ NN_Layer NN_create_layer(int neurons, int outneurons, double* input_buffer, doub
     layer.weights = matrix_create(outneurons, neurons, weight_buffer);
     layer.biases = matrix_create(outneurons, 1, bias_buffer);
     layer.outputs = matrix_create(outneurons, 1, output_buffer);
+    layer.activation = activation;
+    layer.activation_derivative = activation_derivative;
     return layer;
 }
 
@@ -53,13 +55,13 @@ NN_Layer NN_forward(const NN_Layer layer) {
     return layer;
 }
 
-Neural_Network NN_forward_pass(const Neural_Network net, int (*activation_func)(double)) {
+Neural_Network NN_forward_pass(const Neural_Network net) {
     Neural_Network current_net = net;
     for (int i = 0; i < net.num_layers; i++) {
         current_net.layers[i] = NN_forward(current_net.layers[i]);
         if (i < net.num_layers - 1) {
             for (int r = 0; r < current_net.layers[i].outneurons; r++) {
-                double activated_value = activation_func(matrix_get_entry(current_net.layers[i].outputs, r, 0));
+                double activated_value = current_net.layers[i].activation(matrix_get_entry(current_net.layers[i].outputs, r, 0));
                 matrix_set_entry(current_net.layers[i + 1].inputs, r, 0, activated_value);
             }
         }
@@ -67,43 +69,91 @@ Neural_Network NN_forward_pass(const Neural_Network net, int (*activation_func)(
     return current_net;
 }
 
-double NN_ReLU(double x) {
+double ReLU(double x) {
     return (x > 0) ? x : 0;
 }
-double NN_Sigmoid(double x) {
-    return 1.0 / (1.0 + exp(-x));
-}
-double NN_Tanh(double x) {
-    return tanh(x);
-}
-double NN_Linear(double x) {
-    return x;
+double ReLU_1(double x) {
+    return (x > 0) ? 1 : 0;
 }
 
-NN_Layer NN_output_backpropagation(const NN_Layer layer, const Matrix target, double learning_rate) {
+double Sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+double Sigmoid_1(double x) {
+    double sig = Sigmoid(x);
+    return sig * (1 - sig);
+}
+
+double Tanh(double x) {
+    return tanh(x);
+}
+double Tanh_1(double x) {
+    return (1 / cosh(x))*(1 / cosh(x));
+}
+
+double Linear(double x) {
+    return x;
+}
+double Linear_1(double x) {
+    return 1;
+}
+
+NN_Layer NN_output_delta(const NN_Layer layer, const Matrix target) {
     for (int i = 0; i < layer.outneurons; i++) {
         double output = matrix_get_entry(layer.outputs, i, 0);
         double output_activated = layer.activation(output);
         double delta = (output_activated - matrix_get_entry(target, i, 0)) * layer.activation_derivative(output);
         matrix_set_entry(layer.deltas, i, 0, delta);
     }
+    return layer;
+}
 
-    //------------------
-    // UNCHECKED CODE
-    //------------------
-    // Update weights and biases
-    for (int r = 0; r < layer.outneurons; r++) {
-        for (int c = 0; c < layer.neurons; c++) {
-            double weight = matrix_get_entry(layer.weights, r, c);
-            double input = matrix_get_entry(layer.inputs, c, 0);
-            double delta = matrix_get_entry(layer.deltas, r, 0);
-            weight += learning_rate * delta * input;
-            matrix_set_entry(layer.weights, r, c, weight);
+NN_Layer NN_hidden_delta(const NN_Layer layer, const NN_Layer next_layer) {
+    for (int i = 0; i < layer.outneurons; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < next_layer.outneurons; j++) {
+            double next_delta = matrix_get_entry(next_layer.deltas, j, 0);
+            double weight = matrix_get_entry(next_layer.weights, j, i);
+            sum += next_delta * weight;
         }
-        double bias = matrix_get_entry(layer.biases, r, 0);
-        double delta = matrix_get_entry(layer.deltas, r, 0);
-        bias += learning_rate * delta;
-        matrix_set_entry(layer.biases, r, 0, bias);
+        double output = matrix_get_entry(layer.outputs, i, 0);
+        double output_activated = layer.activation(output);
+        double delta = sum * layer.activation_derivative(output);
+        matrix_set_entry(layer.deltas, i, 0, delta);
     }
     return layer;
+}
+
+Neural_Network NN_backward_pass(Neural_Network net, const Matrix target) {
+    Neural_Network current_net = net;
+    for (int i = net.num_layers - 1; i >= 0; i--) {
+        if (i == net.num_layers - 1) {
+            current_net.layers[i] = NN_output_delta(current_net.layers[i], target);
+        } else {
+            current_net.layers[i] = NN_hidden_delta(current_net.layers[i], current_net.layers[i + 1]);
+        }
+    }
+    return current_net;
+}
+
+Neural_Network NN_update_weights(Neural_Network net, double learning_rate) {
+    Neural_Network current_net = net;
+    for (int i = 0; i < net.num_layers; i++) {
+        NN_Layer layer = current_net.layers[i];
+        for (int r = 0; r < layer.outneurons; r++) {
+            for (int c = 0; c < layer.neurons; c++) {
+                double input = matrix_get_entry(layer.inputs, c, 0);
+                double delta = matrix_get_entry(layer.deltas, r, 0);
+                double weight = matrix_get_entry(layer.weights, r, c);
+                double new_weight = weight - learning_rate * delta * input;
+                matrix_set_entry(layer.weights, r, c, new_weight);
+            }
+            double bias = matrix_get_entry(layer.biases, r, 0);
+            double delta = matrix_get_entry(layer.deltas, r, 0);
+            double new_bias = bias - learning_rate * delta;
+            matrix_set_entry(layer.biases, r, 0, new_bias);
+        }
+        current_net.layers[i] = layer;
+    }
+    return current_net;
 }
